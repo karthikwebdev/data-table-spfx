78affea482a6846b0042615e5308bd590edc717c
E.Karthik <kartheekenumarthi@gmail.com>
2021-01-15 12:23:51 +0530

    first commit

---
+++ src/webparts/listItemsHooks/components/TruncatedText.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { useState } from "react";
+
+interface Props{
+  text:string
+}
+
+function TruncatedText({ text }:Props) {
+
+  const [isTextOpen, setIsTextOpen] = useState<boolean>(false);
+
+  return <span>
+    {
+      isTextOpen ? text : text.slice(0,40)  
+    }
+    <span
+      style={{
+        padding:"20px 0",
+        color:"blue",
+        textDecoration:"underlined",
+        cursor:"pointer"
+      }}
+      onClick={() => setIsTextOpen(prev => !prev)}
+>
+      ...Read { isTextOpen ? "Less" :"More"}
+    </span>
+  </span>;
+}
+
+export default TruncatedText;
